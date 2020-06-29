51689f7412001e88e340b8548d60d697608aee16
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-29 17:34:24 -0300

    feat: criando a rota para page erro

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 import Header from './components/header';
+import Erro from './pages/erro';
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
 			<Header />
 			<Switch>
-				<Route path="/" component={Home} />
+				<Route exact path="/" component={Home} />
+				<Route path="*" component={Erro} />
 			</Switch>
 		</BrowserRouter>
 	);
