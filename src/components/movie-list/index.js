feaf52698a0c970e3e503689ba141ab33ad7fc8a
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-29 17:06:52 -0300

    feat: criando o component movie-list

---
+++ src/components/movie-list/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './index.css';
+
+const MovieList = ({ movies }) => {
+	return (
+		<div className="container">
+			<div className="wrap-movies">
+				<h1 className="title">Movies list</h1>
+				<div className="items">
+					{movies.map((movie) => (
+						<article key={movie.id}>
+							<img src={movie.foto} alt="" />
+							<div className="text">
+								<h1>{movie.nome}</h1>
+								<Link to="/">view more</Link>
+							</div>
+						</article>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MovieList;
