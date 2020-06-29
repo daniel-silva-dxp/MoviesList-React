534613e9a713b3dd570615436fb3b1dd57cf05db
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-29 17:08:08 -0300

    feat: importando o component movie-list na page home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import MovieList from '../../components/movie-list';
 
 class Home extends Component {
+	constructor() {
+		super();
+		this.state = {
+			movies: []
+		};
+		this.getMovies = this.getMovies.bind(this);
+		this.setMovies = this.setMovies.bind(this);
+	}
+
+	async componentDidMount() {
+		this.setMovies(await this.getMovies());
+		console.log(await this.getMovies());
+	}
+
+	async getMovies() {
+		return await fetch('https://sujeitoprogramador.com/r-api/?api=filmes')
+			.then((movie) => movie.json())
+			.then((movie) => movie);
+	}
+
+	setMovies(movies) {
+		this.setState({ movies });
+	}
+
 	render() {
-		return <div>HOME</div>;
+		return <MovieList movies={this.state.movies} />;
 	}
 }
 
