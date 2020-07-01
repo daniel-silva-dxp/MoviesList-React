feabd970e1dda34a90cce88aa7361a59278cfa48
Daniel Silva <dani3l.dxp@gmail.com>
2020-07-01 17:30:32 -0300

    feat: implementando uma rota dinâmica para cada filme

--- src/pages/filme/index.js
+++ src/pages/filme/index.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './index.css';
 
-class ViewMovie extends Component {
+class Filme extends Component {
 	constructor(props) {
-		super();
+		super(props);
 		this.state = {
 			movie: []
 		};
+		this.getMovie = this.getMovie.bind(this);
+		this.setMovie = this.setMovie.bind(this);
 	}
 
-	componentDidMount() {
-		const { id } = this.props.match.params;
-		console.log(id);
+	async componentDidMount() {
+		console.log('AQUI');
+		this.setMovie(await this.getMovie());
 	}
+
+	async getMovie() {
+		const { id } = this.props.match.params;
+		return await fetch(`https://sujeitoprogramador.com/r-api/?api=filmes/${id}`)
+			.then((movie) => movie.json())
+			.then((movie) => movie);
+	}
+
+	setMovie(movie) {
+		this.setState({ movie });
+	}
+
 	render() {
-		return <div className="movie">FILME</div>;
+		return (
+			<div className="container align-items">
+				<div className="filme-info">
+					<img src={this.state.movie.foto} alt={this.state.movie.nome} />
+					<h1> {this.state.movie.nome} </h1>
+					{this.state.movie.length !== 0 && <h3>Sinopse</h3>}
+					<p>{this.state.movie.sinopse}</p>
+					{this.state.movie.length !== 0 && <Link to="/">home</Link>}
+				</div>
+			</div>
+		);
 	}
 }
 
-export default ViewMovie;
+export default Filme;
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 import Header from './components/header';
-import ViewMovie from './pages/filme';
 import Erro from './pages/erro';
+import Filme from './pages/filme';
 
 const Routes = () => {
 	return (
@@ -11,7 +11,7 @@ const Routes = () => {
 			<Header />
 			<Switch>
 				<Route exact path="/" component={Home} />
-				<Route exact path="/movie/:id" compornent={ViewMovie} />
+				<Route exact path="/movie/:id" component={Filme} />
 				<Route path="*" component={Erro} />
 			</Switch>
 		</BrowserRouter>
